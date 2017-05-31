3551b7f31ac7d946879ad7960b6a20fdc3922d36
gizarash <gizarash@gmail.com>
2017-05-31 16:12:41 +0600

    Display post on index.php

--- index.php
+++ index.php
@@ -12,27 +12,39 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
+
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
 
+                <?php
+                $query = "SELECT * FROM posts";
+                $result = mysqli_query($connection, $query);
+                while ($row = mysqli_fetch_assoc($result)) {
+                  $title = $row['title'];
+                  $author = $row['author'];
+                  $date = $row['date'];
+                  $image = $row['image'];
+                  $content = $row['content'];
+                ?>
+
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                    <a href="#"><?php echo $title; ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="#"><?php echo $author; ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $date; ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images/<?php echo $image; ?>" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <?php echo $content; ?>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-
+                <?php } ?>
 
                 <!-- Pager -->
                 <ul class="pager">
